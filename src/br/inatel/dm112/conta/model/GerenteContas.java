86c1abddbff330894f58a92e3e9db3c26de34ed8
Joao Albertino <j.albertino.neto@gmail.com>
2018-03-24 11:52:29 -0300

    second commit

---
+++ src/br/inatel/dm112/conta/model/GerenteContas.java
@@ -0,0 +1,19 @@
+package br.inatel.dm112.conta.model;
+
+import java.util.Map;
+
+public class GerenteContas {
+	 
+	public void atualizarTodasContas() {
+	}
+	
+	public void adicionarConta(Conta conta) {
+		
+	}
+	
+	public void removerConta(Conta conta) {
+			
+	}
+	
+
+}
